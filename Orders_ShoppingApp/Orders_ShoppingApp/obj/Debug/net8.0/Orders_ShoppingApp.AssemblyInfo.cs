b3eba23abf785bf33e705ea3b8198ee201ce311c
KongBerg <128359687+KongBerg@users.noreply.github.com>
2024-09-30 09:46:57 +0200

    efsd

--- Orders_ShoppingApp/Orders_ShoppingApp/obj/Debug/net8.0/Orders_ShoppingApp.AssemblyInfo.cs
+++ Orders_ShoppingApp/Orders_ShoppingApp/obj/Debug/net8.0/Orders_ShoppingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Orders_ShoppingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4cb9c80b6d8114414ae54d00568570d62f243c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Orders_ShoppingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Orders_ShoppingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
